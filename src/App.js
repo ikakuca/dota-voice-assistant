a9c5e996e2ebb97fc5c3f0ebbdf9ec3c05251037
ikakuca <ikakuca@gmail.com>
2020-07-10 00:02:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [timerRuning, toggleTimer] = useState(false);
+  const [seconds, setSeconds] = useState(58);
+  const [minutes, setMinutes] = useState(0);
+
+  useEffect(() => {
+    if (timerRuning) {
+      const timout = setTimeout(() => {
+        if (seconds >= 59) {
+          setSeconds(0);
+          setMinutes((prevMinutes) => prevMinutes + 1);
+        } else {
+          setSeconds((prev) => prev + 1);
+        }
+      }, 1000);
+
+      return () => clearTimeout(timout);
+    }
+  }, [seconds, timerRuning]);
+
+  function startTimer() {
+    toggleTimer(true)
+  }
+
+  function resetTimer() {
+    toggleTimer(false);
+    setSeconds(0);
+    setMinutes(0);
+  }
+
+  function pauseTimer() {
+    toggleTimer(false);
+  }
+
+  function subSeconds() {}
+
+  function addSeconds() {}
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +55,37 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <br />
+      <br />
+      <br />
+
+      <input value={minutes} onFocus={pauseTimer} />
+      <input value={seconds} onFocus={pauseTimer} />
+
+      <br />
+      <br />
+
+      <button type="button" onClick={subSeconds}>
+        SUB 1
+      </button>
+
+      <button type="button" onClick={startTimer}>
+        START
+      </button>
+
+      <button type="button" onClick={pauseTimer}>
+        PAUSE
+      </button>
+
+      <button type="button" onClick={resetTimer}>
+        RESET
+      </button>
+
+      <button type="button" onClick={addSeconds}>
+        ADD 1
+      </button>
     </div>
   );
 }
